dc7398c08a6107c82be4c9c0762565fa8df31efc
lhhyung91 <lhhyung91@gmail.com>
2021-04-05 13:58:39 +0900

    [기능] 배경 랜덤 이미지 APi 기능 구현

--- src/components/BackGround/getList.js
+++ src/components/BackGround/getList.js
@@ -1,6 +1,3 @@
-// import React from 'react'
-// import axios from 'axios';
-
 export default async function getList() {
   const apiAddress = 'https://api.unsplash.com';
   const accessKey = 'GX6cJXQnXXo3jOzqeDvkUaD24XuuF6XBbadQ3siLF-0';
@@ -8,13 +5,5 @@ export default async function getList() {
     `${apiAddress}/photos/random?client_id=${accessKey}&query=nature&count=1`,
   );
   const photo = res.json();
-  console.log(res);
-  console.log(photo.urls);
-  console.log(photo);
-
   return photo;
 }
-
-// setInterval(() => {
-//   getList();
-// }, 5000);
