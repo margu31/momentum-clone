79c13d94b3d6101d015141d1fa4fb789a3d12410
hyorard-b <reddream08@gmail.com>
2021-04-05 11:06:27 +0900

    [기능] 로그인, 회원가입 컨테이너 컴포넌트 추가

---
+++ src/containers/SignContainer/SignContainer.styled.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledSignContainer = styled.div`
+  display: inline-flex;
+  position: absolute;
+  top: 1rem;
+  right: 5rem;
+  padding: 1rem;
+`;
+
+export default StyledSignContainer;
